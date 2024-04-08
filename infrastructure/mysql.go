9e23c336b20bc0692f426cee5f44738fb8b49791
Jonny <ji394su311203106@gmail.com>
2024-04-09 00:58:07 +0800

    feat: rename db global name

--- infrastructure/mysql.go
+++ infrastructure/mysql.go
@@ -5,10 +5,10 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var Db *gorm.DB
 
 func InitMySQL() (err error) {
-	db, err = gorm.Open(mysql.New(mysql.Config{
+	Db, err = gorm.Open(mysql.New(mysql.Config{
 		//DSN:        "admin:1234@tcp(mysql80:3306)/gorm?charset=utf8&parseTime=True&loc=Local", // data source name, 详情参考：https://github.com/go-sql-driver/mysql#dsn-data-source-name
 		DSN: "admin:1234@tcp(localhost:3306)/be103?charset=utf8&parseTime=True&loc=Local", // data source name, 详情参考：https://github.com/go-sql-driver/mysql#dsn-data-source-name
 	}), &gorm.Config{})
