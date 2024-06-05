b1cf5ec9bb00ec9ed5b7eae193d7b709c35c30e0
neal <neal0420yang@gmail.com>
2024-06-05 23:07:18 +0800

    user router

--- route/gin.go
+++ route/gin.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"shopping-cart/handler/general"
 	"shopping-cart/handler/post"
+	"shopping-cart/handler/user"
 )
 
 func InitGinServer() (server *gin.Engine, err error) {
@@ -15,10 +16,17 @@ func InitGinServer() (server *gin.Engine, err error) {
 func GinRouter() (server *gin.Engine) {
 	server = gin.New()
 
+	server.LoadHTMLGlob("frontend/*")
+
+	api := server.Group("/api")
+	//orderGroup := api.Group("/orders")
+	//userGroup := api.Group("/auth")
+
 	group := server.Group("")
 
 	post.NewPosts(group)
 	general.NewGeneral(group)
+	user.NewAuthorization(api)
 
 	return server
 }
