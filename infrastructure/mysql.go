be99e9cbdb5395be42f3a3f0d958487e7c4a2b4e
neal <neal0420yang@gmail.com>
2024-06-05 22:57:16 +0800

    mysql utf8 fixed

--- infrastructure/mysql.go
+++ infrastructure/mysql.go
@@ -10,7 +10,7 @@ var Db *gorm.DB
 func InitMySQL() (err error) {
 	Db, err = gorm.Open(mysql.New(mysql.Config{
 		//DSN:        "admin:1234@tcp(mysql80:3306)/gorm?charset=utf8&parseTime=True&loc=Local", // data source name, 详情参考：https://github.com/go-sql-driver/mysql#dsn-data-source-name
-		DSN: "admin:1234@tcp(localhost:3306)/be103?charset=utf8&parseTime=True&loc=Local", // data source name, 详情参考：https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DSN: "admin:1234@tcp(localhost:3306)/be103?charset=utf8mb4&parseTime=True&loc=Local", // data source name, 详情参考：https://github.com/go-sql-driver/mysql#dsn-data-source-name
 	}), &gorm.Config{})
 	return
 }
